5d2123275807196a8972aa3bc5de00a4d671e10d
Q P <qp@Qs-MacBook-Air.local>
2023-03-16 11:34:06 +0300

    0x17

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string): 
 	roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-	int_string = list(map(lambda a: roman[a], list(roman_string)))
-	for i in range(0, len(int_string) - 1 ):
-		if int_string[i] < int_string[i+1]:
-			int_string[i] *= -1
-	return sum(int_string)
+	ints = list(map(lambda a: roman[a], list(roman_string)))
+	weights = list(map(lambda a: -1 if a[0] < a[1]  else 1, list(zip(ints[:-1], ints[1:])))) + [1]
+	return sum(map(lambda a: a[0] * a[1], zip(ints, weights)))
