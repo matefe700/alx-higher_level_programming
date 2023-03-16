00653d0a65af0094abc81b369aec9e910b4758a3
Q P <qp@Qs-MacBook-Air.local>
2023-03-16 06:10:30 +0300

    0x04

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,6 @@
 def weight_average(my_list=[]):
     if len(my_list) is 0:
         return 0
-    a = list(map(list, zip(*my_list)))
-    b = [x * y for x, y in zip(a[0], a[1])]
-    return sum(b) / sum(a[1])
+    a = sum(map(lambda x: x[0] * x[1], my_list))
+    b = sum(map(lambda x: x[1], my_list))
+    return a / b
