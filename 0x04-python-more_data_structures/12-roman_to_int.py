15243e6bea9f996a9b99aec7813cc9022b7fbfa7
Q P <qp@Qs-MacBook-Air.local>
2023-03-16 06:29:36 +0300

    0x04

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,8 @@
-12-roman_to_int.py
+#!/usr/bin/python3
+def roman_to_int(roman_string): 
+	roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+	int_string = list(map(lambda a: roman[a], list(roman_string)))
+	for i in range(0, len(int_string) - 1 ):
+		if int_string[i] < int_string[i+1]:
+			int_string[i] *= -1
+	return sum(int_string)
