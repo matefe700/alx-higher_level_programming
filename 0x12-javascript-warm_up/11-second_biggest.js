7d3eab3d5cff0bdcc29c3fe465acb2e9f12f04a9
Q P <qp@Qs-MacBook-Air.local>
2023-07-02 22:03:22 +0100

    alx

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
-if (process.argv.length < 2) {
-  console.log(0);
+if (process.argv.length < 4) {
+  console.log('0');
 } else {
   const num = process.argv.slice(2).map(x => parseInt(x));
-  num.sort()[num.length - 2];
+  num.sort(function (a, b) { return b - a; });
+  console.log(num[1]);
 }
