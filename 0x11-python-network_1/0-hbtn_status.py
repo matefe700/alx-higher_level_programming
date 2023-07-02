0e702e79df88cb1f46f8e66df68a7556ce863583
Q P <qp@Qs-MacBook-Air.local>
2023-07-02 12:47:58 +0100

    0x11

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status"""
-import urllib.request as request
-
+"""fetches
+   https://intranet.hbtn.io/status"""
+import urllib.request
 
 if __name__ == "__main__":
-    with request.urlopen('https://alx-intranet.hbtn.io/status') as r:
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as r:
         html = r.read()
-        print('Body response:')
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+        print("Body response:")
+        print(f"\t- type: {type(html)}")
+        print(f"\t- content: {html}")
+        print(f"\t- utf8 content: {html.decode('utf-8')}")
