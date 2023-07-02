dfe218cbd7ac0af1b601a61f18cfb7f8b94d53bc
Q P <qp@Qs-MacBook-Air.local>
2023-07-02 17:54:13 +0100

    ALX

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,8 @@
 import urllib.request
 
 if __name__ == "__main__":
-    url = "https://intranet.alxswe.com/status"
-    with urllib.request.urlopen(url) as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
-        print(f"\t- type: {type(html)}")
-        print(f"\t- content: {html}")
-        print(f"\t- utf8 content: {html.decode('utf-8')}")
+        print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+              .format(type(html), html, html.decode('utf-8')))
